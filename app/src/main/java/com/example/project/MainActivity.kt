58097323e5c669a410d8dcb36513c7aa3159c1ff
haeseong.oh <haeseong.oh@naverz-corp.com>
2024-02-20 18:28:56 +0900

    test commit

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.project.VO.feedbackVO
 import com.example.project.VO.datemailVO
+import com.example.project.VO.feedbackVO
 import com.example.project.databinding.ActivityMainBinding
+import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
+
 
 class MainActivity : AppCompatActivity(), OnItemListener {
     lateinit var binding: ActivityMainBinding
@@ -33,21 +35,21 @@ class MainActivity : AppCompatActivity(), OnItemListener {
         setMonthView()
 
         // 이전달 버튼 이벤트
-        binding.preBtn.setOnClickListener{
+        binding.preBtn.setOnClickListener {
             // 현재 월 -1 변수에 담기
             selectedDate = selectedDate.minusMonths(1)
             setMonthView()
         }
 
         // 다음달 버튼 이벤트
-        binding.nextBtn.setOnClickListener{
+        binding.nextBtn.setOnClickListener {
             selectedDate = selectedDate.plusMonths(1)
             setMonthView()
         }
     }
 
     // 날짜 화면에 보여주기
-    private fun setMonthView(){
+    private fun setMonthView() {
         // 년월 텍스트뷰 세팅
         binding.monthYearText.text = monthYearFromDate(selectedDate)
 
@@ -71,16 +73,16 @@ class MainActivity : AppCompatActivity(), OnItemListener {
     }
 
     // 날짜 타입 설정(월,년)
-    private fun monthYearFromDate(date: LocalDate):String{
+    private fun monthYearFromDate(date: LocalDate): String {
 
         var formatter = DateTimeFormatter.ofPattern("MM월 yyyy년")
 
         // 받아온 날짜를 해당 포맷으로 변경
         return date.format(formatter)
     }
-    
-    // 날짜 타입 설정(년,월)
-    private fun yearMonthFromDate(date: LocalDate):String{
+
+    // 날짜 타입 설정(년,월)ㄴ
+    private fun yearMonthFromDate(date: LocalDate): String {
 
         var formatter = DateTimeFormatter.ofPattern("yyyy년 MM월")
 
@@ -89,7 +91,7 @@ class MainActivity : AppCompatActivity(), OnItemListener {
     }
 
     // 검색용 날짜 타입 생성(yyyy-mm-dd)
-    private fun searchingFromDate(date: LocalDate, dayText: String):String{
+    private fun searchingFromDate(date: LocalDate, dayText: String): String {
         var formatter = DateTimeFormatter.ofPattern("yyyy-MM")
         var result = date.format(formatter) + "-" + dayText
 
@@ -97,14 +99,14 @@ class MainActivity : AppCompatActivity(), OnItemListener {
     }
 
     // 검색용 날짜 타입 생성(yyyy-mm)
-    private fun searchingFromMonth(date: LocalDate):String{
+    private fun searchingFromMonth(date: LocalDate): String {
         var formatter = DateTimeFormatter.ofPattern("yyyy-MM")
 
         return date.format(formatter)
     }
 
     // 해당 달의 일 리스트 생성
-    private fun dayInMonthArray(date: LocalDate):ArrayList<String>{
+    private fun dayInMonthArray(date: LocalDate): ArrayList<String> {
         var dayList = ArrayList<String>()
 
         var yearMonth = YearMonth.from(date)
@@ -117,11 +119,11 @@ class MainActivity : AppCompatActivity(), OnItemListener {
 
         // 첫 번째 날 요일 맞추기(월:1, 일:7)
         var dayOfWeek = firstDay.dayOfWeek.value
-        for(i in 1..41){
-            if(i <= dayOfWeek || i > (lastDay + dayOfWeek)){
+        for (i in 1..41) {
+            if (i <= dayOfWeek || i > (lastDay + dayOfWeek)) {
                 dayList.add("")
-            }else{
-                dayList.add((i-dayOfWeek).toString())
+            } else {
+                dayList.add((i - dayOfWeek).toString())
             }
         }
 
@@ -129,7 +131,7 @@ class MainActivity : AppCompatActivity(), OnItemListener {
     }
 
     // 해당 달의 감정 불러와서 리스트로 만들기
-    private fun monthEmotionArray(date: String, email: String):ArrayList<feedbackVO>{
+    private fun monthEmotionArray(date: String, email: String): ArrayList<feedbackVO> {
         var emotionList = ArrayList<feedbackVO>()
 
 
@@ -138,16 +140,16 @@ class MainActivity : AppCompatActivity(), OnItemListener {
 
 
 
-        emotionList.add(feedbackVO(1,2, "기쁨","ai피드백", "2024-02-14"))
+        emotionList.add(feedbackVO(1, 2, "기쁨", "ai피드백", "2024-02-14"))
 
         return emotionList
     }
-    
+
     // 아이템 클릭 이벤트
-    override fun onItemClick(dayText: String){
+    override fun onItemClick(dayText: String) {
         // 선택한 날짜를 yyyy-MM-DD 형태로 변경
         var searching = searchingFromDate(selectedDate, dayText)
-        
+
         // 클릭한 날짜의 토스트 메세지 띄우기
         Toast.makeText(this, searching, Toast.LENGTH_SHORT).show()
     }
@@ -163,7 +165,7 @@ class MainActivity : AppCompatActivity(), OnItemListener {
                 call: Call<List<feedbackVO>>,
                 response: Response<List<feedbackVO>>
             ) {
-                if(response.isSuccessful()){ // 응답을 잘 받은 경우
+                if (response.isSuccessful) { // 응답을 잘 받은 경우
                     Log.d("RESPONSE: ", response.body().toString())
                 } else { // 통신은 성공했지만 응답에 문제가 있는 경우
                     Log.d("RESPONSE ERROR: ", "2")
