16c486b065eaf8192226080446c9798596c8ac72
김영준 <4upflej@gmail.com>
2024-02-21 13:53:21 +0900

    영준 - 커밋

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.kakao.sdk:v2-all:2.15.0")
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
