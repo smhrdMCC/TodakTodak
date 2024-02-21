6f49a04ed60d1b36c6dc141edcb5391e5bd96dda
김영준 <4upflej@gmail.com>
2024-02-21 09:25:06 +0900

    피드백 추가

--- app/src/main/java/com/todaktodak/model/Feedback.kt
+++ app/src/main/java/com/todaktodak/model/Feedback.kt
@@ -1,3 +1,18 @@
 package com.todaktodak.model
 
-data class Feedback()
+class Feedback {
+    var prompt: String? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+    var feedback: String? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+}
--- app/src/main/java/com/todaktodak/view/FeedbackActivity.kt
+++ app/src/main/java/com/todaktodak/view/FeedbackActivity.kt
@@ -1,12 +1,92 @@
 package com.todaktodak.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.todaktodak.R
+import com.todaktodak.databinding.ActivityFeedbackBinding
+import com.todaktodak.model.Feedback
+import com.todaktodak.retrofit.RetrofitBuilder
+import com.todaktodak.retrofit.RetrofitBuilder2
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FeedbackActivity : AppCompatActivity() {
+    lateinit var binding: ActivityFeedbackBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feedback)
+        binding = ActivityFeedbackBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var intent: Intent = getIntent()
+
+        var result = intent.getStringExtra("data")
+
+        val feedback = Feedback()
+        feedback.prompt = result
+        lifecycleScope.launch { Dispatchers.IO
+            Feed(feedback.prompt.toString())
+            delay(10000)
+            feedback.feedback = binding.textView.text.toString()
+            backFeed(feedback.feedback.toString() + ":" + feedback.prompt.toString())
+        }
+
+        Log.d("FEED",feedback.feedback.toString())
+        Log.d("FEED_Prompt",feedback.prompt.toString())
+    }
+
+    suspend fun Feed(prompt: String){
+        val call = RetrofitBuilder.api.updateFeedResponse(prompt)
+
+        call.enqueue(object : Callback<String> { // 비동기 방식 통신 메소드
+            override fun onResponse( // 통신에 성공한 경우
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                if(response.isSuccessful()){ // 응답 잘 받은 경우
+                    Log.d("RESPONSE: ", "성공!"+response.body().toString())
+                    binding.textView.text=response.body().toString()
+
+                }else{
+                    // 통신 성공 but 응답 실패
+
+                    Log.d("RESPONSE", "FAILURE")
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                // 통신에 실패한 경우
+                Log.d("CONNECTION FAILURE: ", t.localizedMessage)
+            }
+        })
+    }
+    suspend fun backFeed(feedback: String){
+        val call = RetrofitBuilder2.api.getFeedResponse(feedback)
+
+        call.enqueue(object : Callback<String> { // 비동기 방식 통신 메소드
+            override fun onResponse( // 통신에 성공한 경우
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                if(response.isSuccessful()){ // 응답 잘 받은 경우
+                    Log.d("RESPONSE: ", "피드백 인서트 성공!"+response.body().toString())
+
+                }else{
+                    // 통신 성공 but 응답 실패
+                    Log.d("RESPONSE", "피드백 인서트 FAILURE")
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                // 통신에 실패한 경우
+                Log.d("CONNECTION FAILURE: ", t.localizedMessage)
+            }
+        })
     }
 }
