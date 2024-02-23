e4ef31fd13b82ded96e05095517b68852978a3d0
Junseong <monyuaid@gmail.com>
2024-02-23 13:43:33 +0900

    머지중

--- app/src/main/java/com/todaktodak/view/StartActivity.kt
+++ app/src/main/java/com/todaktodak/view/StartActivity.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.todaktodak.R
+import com.todaktodak.retrofit.usersingleton
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +17,10 @@ class StartActivity : AppCompatActivity() {
         var check = sharedPreferences.getString("userId", "none")
 
         if(check == "none"){
-            // 통합 후 카카오 로그인으로 경로 지정하기 ########################################################
-            var goKakao = Intent(this, DiaryListActivity::class.java)
+            var goKakao = Intent(this, MainActivity_kakao::class.java)
             startActivity(goKakao)
         }else{
-            // 통합 후 유저 싱글톤에 아이디 넣기 #############################################################
+            usersingleton.userEmail = check!!
             var goCalendar = Intent(this, CalendarActivity::class.java)
             startActivity(goCalendar)
         }
